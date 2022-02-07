39f2855f3e3331c8b0b24440a97d46beb45e4ebf
Crowdbotics <engineering@crowdbotics.com>
2022-02-07 10:35:09 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Home(models.Model):
+    "Generated Model"
+    address = models.BigIntegerField()
--- backend/testappauto20_dev_23504/wsgi.py
+++ backend/testappauto20_dev_23504/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testappauto20_dev_23504.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testappauto20_dev_23504.settings")
 
 application = get_wsgi_application()
